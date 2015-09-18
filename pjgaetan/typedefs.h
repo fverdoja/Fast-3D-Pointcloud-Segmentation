228df8b2134a9753056900a05191c25744480692
Francesco Verdoja <f.verdoja@gmail.com>
2015-09-18 15:01:04 +0200

    Removed Florian's legacy code

--- pjgaetan/typedefs.h
+++
@@ -1,9 +0,0 @@
-//define LONG, USHORT and BYTE (from windows/visual studio. why not just use standard types anyway!?)
-#ifndef typedef_gaetan
-#define typedef_gaetan
-
-typedef char BYTE;                 // Pls microsoft
-typedef unsigned short USHORT;     // Y u no comply to standards
-typedef double LONG;               // microsoft pls stahp
-
-#endif
